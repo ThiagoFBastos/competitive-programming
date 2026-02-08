b3dc2e925640b25568609f44c522d2d886801aad
Thiago Bastos <thiagobastos1705@gmail.com>
2026-02-08 17:41:36 -0300

    refactor: prettify the solution of Factorize

--- yosupo/Factorize.cpp
+++ yosupo/Factorize.cpp
@@ -2,32 +2,20 @@
  
 using namespace std;
  
-#define INF 1000000000
-#define INFLL 1000000000000000000ll
-#define EPS 1e-9
-#define all(x) x.begin(),x.end()
-#define rall(x) x.rbegin(),x.rend()
-#define pb push_back
-#define fi first
-#define sc second
- 
-using i64 = long long;
-using u64 = unsigned long long;
-using ld = long double;
-using ii = pair<int, int>;
-using i128 = __int128;
- 
-namespace factorization {
- 
+namespace factorization
+{ 
 	template<class T>
-	T modMul(T a, T b, T m) {
+	T modMul(T a, T b, T m)
+	{
 		return (T)((__int128)a * b % m);
 	}
  
 	template<class T>
-	T modMulExp(T a, T p, T m) {
+	T modMulExp(T a, T p, T m)
+	{
 		T ans = 1;
-		for(; p > 0; p >>= 1) {
+		for(; p > 0; p >>= 1) 
+		{
 			if(p & 1) ans = modMul(ans, a, m);
 			a = modMul(a, a, m);
 		}
@@ -35,7 +23,8 @@ namespace factorization {
 	}
  
 	template<class T>
-	bool miller(T n) {
+	bool miller(T n)
+	{
 		const int pn = 9;
 		const T p[] = {2, 3, 5, 7, 11, 13, 17, 19, 23};
  
@@ -50,14 +39,16 @@ namespace factorization {
  
 		while(~t & 1) t >>= 1, ++s;
  
-		for(T k : p) {
+		for(T k : p)
+		{
 			T pt = modMulExp(k, t, n);
  
 			if(pt == 1) continue;
  
 			bool ok = false;
  
-			for(int j = 0; j < s && !ok; ++j) {
+			for(int j = 0; j < s && !ok; ++j)
+			{
 				ok = ok || pt == n - 1;
 				pt = modMul(pt, pt, n);
 			}
@@ -69,34 +60,45 @@ namespace factorization {
 	}
  
 	template<class T>
-	T f(T x, T c, T mod) {
+	T f(T x, T c, T mod)
+	{
 		return (modMul(x, x, mod) + c) % mod;
 	}
  
 	template<class T>
-	T rho(T n) {
+	T rho(T n)
+	{
 		mt19937 rnd(std::chrono::system_clock::now().time_since_epoch().count());
 		T x, y, g = n;
-		while(g == n) {
+
+		while(g == n)
+		{
 			T x0 = rnd(), c = rnd();
 			x = y = x0;
 			g = 1;
-			while(g == 1) {
+
+			while(g == 1)
+			{
 				x = f(x, c, n);
 				y = f(f(y, c, n), c, n);
 				g = gcd(abs(x - y), n);
 			}
 		}
+
 		return g;
 	}
 	
 	template<class T>
-	vector<pair<T, int>> factorize(T n) {	
+	auto factorize(T n)
+	{	
 		vector<pair<T, int>> f;
 		
-		if(~n & 1) {
+		if(~n & 1)
+		{
 			int k = 0;
-			while(~n & 1) {
+
+			while(~n & 1)
+			{
 				n >>= 1;
 				++k;
 			}
@@ -104,16 +106,20 @@ namespace factorization {
 			f.emplace_back(2, k);
 		}
 
-		for(T p = 3; p * p * p * p <= n; p += 2) {
+		for(T p = 3; p * p * p * p <= n; p += 2)
+		{
 			if(n % p) continue;
+
 			int i = 0;
 			while(n % p == 0) n /= p, ++i;
-			f.pb({p, i});
+			f.emplace_back(p, i);
 		}
 
-		while(n != 1) {
-			if(miller(n)) {
-				f.pb({n, 1});
+		while(n != 1)
+		{
+			if(miller(n))
+			{
+				f.emplace_back(n, 1);
 				break;
 			}
 			
@@ -124,25 +130,29 @@ namespace factorization {
 
 			int i = 0;
 			while(n % k == 0) n /= k, ++i;
-			f.pb({k, i});
+			f.emplace_back(k, i);
 		}
+
 		return f;
 	}
 };
- 
-void solve() {
+
+int main() {
+	ios_base :: sync_with_stdio(false);
+	cin.tie(0);
+	
 	int q;
 
 	cin >> q;
 
 	while(q-- > 0) {
-		i64 n;
+		long long n;
  
 		cin >> n;
  
-		auto f = factorization :: factorize(n);
+		auto f = factorization::factorize(n);
  
-		sort(all(f));
+		sort(f.begin(), f.end());
  
 		auto power = f | views::transform([](auto p) { return p.second;});
 
@@ -157,13 +167,6 @@ void solve() {
 
 		cout << '\n';
 	}
-}
- 
-int main() {
-	ios_base :: sync_with_stdio(false);
-	cin.tie(0);
-	int t = 1;
-//	cin >> t;
-	while(t--) solve();
+	
 	return 0;
 } 
