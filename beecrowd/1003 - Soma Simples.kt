6bbee7c261b2478c576dfde26a600c61740d2a28
Thiago Felipe Bastos da Silva <thiagobastos1705@gmail.com>
2026-03-13 19:05:43 -0300

    add: 1003 - Soma Simples

---
+++ beecrowd/1003 - Soma Simples.kt
@@ -0,0 +1,11 @@
+import java.util.*
+
+fun main(args: Array<String>) {
+
+	val first_number = readLine()!!.toInt()
+    val second_number = readLine()!!.toInt()
+
+    val sum = first_number + second_number
+
+    println("SOMA = $sum")
+}
