82a6e485a340c11ad44cb5eb3fca926502cbbac9
Thiago Felipe Bastos da Silva <thiagobastos1705@gmail.com>
2026-03-13 19:03:22 -0300

    add: 1001 - Extremamente Básico

---
+++ beecrowd/1001 - Extremamente Básico.kt
@@ -0,0 +1,14 @@
+import java.util.*
+
+fun main(args: Array<String>) {
+
+    val sc = Scanner(System.`in`)
+    
+	val first_num = sc.next().toInt()
+    val second_num = sc.next().toInt()
+
+    val sum = first_num + second_num
+
+    println("X = $sum")
+	
+}
