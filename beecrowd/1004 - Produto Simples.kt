2265980a725556a2fd7aba8ef055c15f887a6fd9
Thiago Felipe Bastos da Silva <thiagobastos1705@gmail.com>
2026-03-13 19:06:44 -0300

    add: 1004 - Produto Simples

---
+++ beecrowd/1004 - Produto Simples.kt
@@ -0,0 +1,11 @@
+import java.util.*
+
+fun main(args: Array<String>) {
+
+val first_number = readLine()!!.toInt()
+    val second_number = readLine()!!.toInt()
+
+    val product = first_number * second_number
+
+    println("PROD = $product")
+}
